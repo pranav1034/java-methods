1efb531ee771bfa24b58bcc71237cc83160f33bc
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-04 23:07:00 +0530

    [Pranav] ADD second-commit-for-second-problem-of-unit-conversion

--- UnitConverter.java
+++ UnitConverter.java
@@ -4,11 +4,16 @@ public class UnitConverter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Unit Converter options:");
+        System.out.println("Unit Converter Menu:");
         System.out.println("1. Kilometers to Miles");
         System.out.println("2. Miles to Kilometers");
         System.out.println("3. Meters to Feet");
         System.out.println("4. Feet to Meters");
+        System.out.println("5. Yards to Feet");
+        System.out.println("6. Feet to Yards");
+        System.out.println("7. Meters to Inches");
+        System.out.println("8. Inches to Meters");
+        System.out.println("9. Inches to Centimeters");
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
 
@@ -33,32 +38,74 @@ public class UnitConverter {
                 result = convertFeetToMeters(value);
                 System.out.println(value + " feet = " + result + " meters");
                 break;
+            case 5:
+                result = convertYardsToFeet(value);
+                System.out.println(value + " yards = " + result + " feet");
+                break;
+            case 6:
+                result = convertFeetToYards(value);
+                System.out.println(value + " feet = " + result + " yards");
+                break;
+            case 7:
+                result = convertMetersToInches(value);
+                System.out.println(value + " meters = " + result + " inches");
+                break;
+            case 8:
+                result = convertInchesToMeters(value);
+                System.out.println(value + " inches = " + result + " meters");
+                break;
+            case 9:
+                result = convertInchesToCentimeters(value);
+                System.out.println(value + " inches = " + result + " cm");
+                break;
             default:
                 System.out.println("Invalid choice!");
         }
+
     }
 
-    // Method to convert kilometers to miles
     public static double convertKmToMiles(double km) {
         double km2miles = 0.621371;
         return km * km2miles;
     }
 
-    // Method to convert miles to kilometers
     public static double convertMilesToKm(double miles) {
         double miles2km = 1.60934;
         return miles * miles2km;
     }
 
-    // Method to convert meters to feet
     public static double convertMetersToFeet(double meters) {
         double meters2feet = 3.28084;
         return meters * meters2feet;
     }
 
-    // Method to convert feet to meters
     public static double convertFeetToMeters(double feet) {
         double feet2meters = 0.3048;
         return feet * feet2meters;
+    }
+    
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    public static double convertInchesToCentimeters(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
     }
 }
